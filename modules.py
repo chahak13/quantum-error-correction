e8db3f748b9bc676ef5c9a346e08f672e3ae1068
chahak13 <chahakmehta013@gmail.com>
2018-11-05 19:16:26 +0530

    Added 1 qubit bit flip circuit

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import Aer, execute
 from qiskit.tools.visualization import circuit_drawer
+import math
 
 def bitFlipError(stateVector = []):
     '''
@@ -8,13 +9,47 @@ def bitFlipError(stateVector = []):
 
         Note: Add compatibility of user given arbitrary state vector in the first qubit.
     '''
-    q = QuantumRegister(3)
-    c = QuantumRegister(3)
+    q = QuantumRegister(3, name = 'q')
+    c = ClassicalRegister(3, name = 'c')
 
-    qc = QuantumCircuit(q, c)
+    qc = QuantumCircuit(q, c, name='bit_flip_circuit')
 
+    # Brings the 3 qubit system to the state (a*|000> + b*|111>). a=b=1/sqrt(2)
+    qc.h(q[0])
     qc.cx(q[0], q[1])
     qc.cx(q[0], q[2])
 
-    return qc
+    # Random bit flip error section.
+    qc.x(q[2])
 
+    ancilla = QuantumRegister(2, name = 'a')
+    classical_ancilla = ClassicalRegister(2, name = 'ca')
+    qc.add(ancilla)
+    qc.add(classical_ancilla)
+
+    qc.cx(q[0], ancilla[0])
+    qc.cx(q[1], ancilla[0])
+    qc.cx(q[0], ancilla[1])
+    qc.cx(q[2], ancilla[1])
+
+    qc.measure(ancilla, classical_ancilla)
+
+    backend_sim = Aer.get_backend('qasm_simulator')
+    job_sim = execute(qc, backend_sim)
+    result_sim = job_sim.result()
+
+    print("Simulation: ", result_sim)
+    print(result_sim.get_counts(qc))
+
+    error_bit = list(result_sim.get_counts().keys())[0].split()[0]
+    if error_bit == '11':
+        print("Bit-flip error in the first bit")
+        
+    elif error_bit == '01':
+        print("Bit-flip error in second bit")
+    elif error_bit == '10':
+        print("Bit-flip error in third bit")
+    else:
+        print("No bit-flip error in the code.")
+
+    return q, c, qc
--- test.py
+++ test.py
@@ -9,32 +9,60 @@
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute, Aer
 from qiskit.tools.visualization import circuit_drawer, matplotlib_circuit_drawer
-# Creates a Quantum Register with 2 qubits.
-q = QuantumRegister(1)
+from modules import *
 
-# Creates a Classical Register with 2 bits.
-c = ClassicalRegister(1)
+def test():
+    # Creates a Quantum Register with 2 qubits.
+    def circuit():
+        q = QuantumRegister(1)
 
-# Creates a Quantum Circuit using the quantum and classical registers.
-qc = QuantumCircuit(q, c)
+        # Creates a Classical Register with 2 bits.
+        c = ClassicalRegister(1)
 
-## Performing some operations on the circuit.
-# Apply an H gate on the first qubit and putting this qubit in a superposition state.
-# qc.h(q[0])
-# qc.cx(q[1])
-# Measuring the state of the system
-qc.measure(q, c)
+        # Creates a Quantum Circuit using the quantum and classical registers.
+        qc = QuantumCircuit(q, c)
+        ## Performing some operations on the circuit.
+        # Apply an H gate on the first qubit and putting this qubit in a superposition state.
+        qc.h(q[0])
+        return q,c,qc
+    
+    q,c,qc = circuit()
+    print(qc)
+    # qc.cx(q[1])
+    # Measuring the state of the system
+    qc.measure(q, c)
 
-# backend_sim = Aer.get_backend('qasm_simulator')
-# job_sim = execute(qc, backend_sim)
-# result_sim = job_sim.result()
+    backend_sim = Aer.get_backend('qasm_simulator')
+    job_sim = execute(qc, backend_sim)
+    result_sim = job_sim.result()
 
-# print("Simulation: ", result_sim)
-# print(result_sim.get_counts(qc))
+    print("Simulation: ", result_sim)
+    print(result_sim.get_counts(qc))
 
-# Provide a name to write the diagram to the filesystem
-# circuit_drawer(qc, filename='./bell_circuit.png')
+    # Provide a name to write the diagram to the filesystem
+    # circuit_drawer(qc, filename='./bell_circuit.png')
 
-# Use the return value with show() to display the diagram
-diagram = matplotlib_circuit_drawer(qc)
-diagram.show()
+    # Use the return value with show() to display the diagram
+    diagram = matplotlib_circuit_drawer(qc)
+    diagram.show()
+
+def test_bitflip():
+    q, c, qc = bitFlipError()
+    # import pdb; pdb.set_trace()
+    # Measuring the state of the system
+    # qc.measure(q, c)
+
+    # backend_sim = Aer.get_backend('qasm_simulator')
+    # job_sim = execute(qc, backend_sim)
+    # result_sim = job_sim.result()
+
+    # print("Simulation: ", result_sim)
+    # print(result_sim.get_counts(qc))
+
+    # diagram = matplotlib_circuit_drawer(qc, filename='bit_flip_circuit.png')
+    # diagram.show()
+    
+if __name__ == '__main__':
+    
+    # test()
+    test_bitflip()
