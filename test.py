15612cbacfd0d94efd392f14cd7956bfbae9c58c
chahak13 <chahakmehta013@gmail.com>
2018-11-11 00:39:58 +0530

    Added desiredState command line argument

--- test.py
+++ test.py
@@ -13,44 +13,11 @@ from modules import bitFlipError, phaseFlipError, shorsCode
 import math
 import argparse
 
-def test():
-    # Creates a Quantum Register with 2 qubits.
-    def circuit():
-        q = QuantumRegister(1)
+def test_bitflip(desiredState, error_probability):
 
-        # Creates a Classical Register with 2 bits.
-        c = ClassicalRegister(1)
+    desired_state = desiredState
 
-        # Creates a Quantum Circuit using the quantum and classical registers.
-        qc = QuantumCircuit(q, c)
-        ## Performing some operations on the circuit.
-        # Apply an H gate on the first qubit and putting this qubit in a superposition state.
-        qc.h(q[0])
-        return q,c,qc
-    
-    q,c,qc = circuit()
-    print(qc)
-    # qc.cx(q[1])
-    # Measuring the state of the system
-    qc.measure(q, c)
-
-    backend_sim = Aer.get_backend('qasm_simulator')
-    job_sim = execute(qc, backend_sim)
-    result_sim = job_sim.result()
-
-    print("Simulation: ", result_sim)
-    print(result_sim.get_counts(qc))
-
-    # Provide a name to write the diagram to the filesystem
-    # circuit_drawer(qc, filename='./bell_circuit.png')
-
-    # Use the return value with show() to display the diagram
-    diagram = matplotlib_circuit_drawer(qc)
-    diagram.show()
-
-def test_bitflip():
-    desired_state = [1/2, math.sqrt(3)/2]
-    q, c, qc = bitFlipError(stateVector=[], error_probability=0.8)
+    q, c, qc = bitFlipError(stateVector=desired_state, error_probability=error_probability)
     qc.measure(q,c)
     backend_sim = Aer.get_backend('qasm_simulator')
     job_sim = execute(qc, backend_sim)
@@ -62,10 +29,11 @@ def test_bitflip():
     # diagram.show()
     
 
-def test_phaseflip():
+def test_phaseflip(desiredState, error_probability):
     # desired_state = [1/2, math.sqrt(3)/2]
-    desired_state = [1, 0]
-    q, c, qc = phaseFlipError(stateVector=desired_state, error_probability=0.8)
+    # desired_state = [1, 0]
+    desired_state = desiredState
+    q, c, qc = phaseFlipError(stateVector=desired_state, error_probability=error_probability)
     qc.measure(q, c)
     backend_sim = Aer.get_backend('qasm_simulator')
     job_sim = execute(qc, backend_sim)
@@ -77,10 +45,12 @@ def test_phaseflip():
     # diagram = matplotlib_circuit_drawer(qc, filename='phase_flip_circuit.png')
     # diagram.show()
 
-def test_shorsCode():
+
+def test_shorsCode(desiredState, error_probability):
     # desired_state = [1/2, math.sqrt(3)/2]
-    desired_state = [1, 0]
-    q, c, qc = shorsCode(stateVector=desired_state, error_probability=0.8)
+    # desired_state = [1, 0]
+    desired_state = desiredState
+    q, c, qc = shorsCode(stateVector=desired_state, error_probability=error_probability)
     qc.measure(q, c)
     backend_sim = Aer.get_backend('qasm_simulator')
     job_sim = execute(qc, backend_sim)
@@ -98,11 +68,13 @@ if __name__ == '__main__':
     parser.add_argument('--error_probability', required = False, type = float, default = 0.8, help = "The probability of there being an error in the qubits while transmission.")
     parser.add_argument('--desiredState', nargs = 2, type = float, help = "Amplitudes of the states 0 and 1 in the form of a list.")
     args = parser.parse_args()
-    print(args)
+
+    if args.desiredState is None:
+        args.desiredState = []
 
     if args.error == 'bitflip':
-        test_bitflip()
+        test_bitflip(args.desiredState, args.error_probability)
     elif args.error == 'phaseflip':
-        test_phaseflip()
+        test_phaseflip(args.desiredState, args.error_probability)
     elif args.error == 'shors':
-        test_shorsCode()
+        test_shorsCode(args.desiredState, args.error_probability)
